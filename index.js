1b773ad7e5922340f5145ac2029521c56021496e
nghia_pham_lt <tnghia14111998@gmail.com>
2019-06-30 22:31:09 +0700

    format

--- index.js
+++ index.js
@@ -21,13 +21,17 @@ app.use(bodyParser.json())
 
 
 app.get('/students', (request, response) => {
-    knex('sinhvien').select()
+    knex('sinhvien')
+        .select()
         .then((a) => {
             response.json({ success: true, data: a })
         })
 })
 app.get('/student/:id', (request, response) => {
-    knex.select().from('sinhvien').where({ id: request.params.id })
+    knex
+        .select()
+        .from('sinhvien')
+        .where({ id: request.params.id })
         .join('khoa', 'sinhvien.idKhoa', 'khoa.idk')
         .then((r) => {
             response.send({ success: true, data: r })
@@ -35,14 +39,19 @@ app.get('/student/:id', (request, response) => {
 })
 
 app.post('/subject/:id/register/phase1', (request, response) => {
-    knex('sotinchichophep').select('soTinChi').where({ idSV: request.headers.authorization })
+    knex('sotinchichophep')
+        .select('soTinChi')
+        .where({ idSV: request.headers.authorization })
         .then(a => {
-            knex('monhoc').select('soTinChi').where({ id: request.params.id })
+            knex('monhoc').select('soTinChi')
+                .where({ id: request.params.id })
                 .then(b => {
                     if (a[0].soTinChi >= b[0].soTinChi) {
-                        knex('dangkidot1').insert({ idMonHoc: request.params.id, idSV: request.headers.authorization })
+                        knex('dangkidot1')
+                            .insert({ idMonHoc: request.params.id, idSV: request.headers.authorization })
                             .then(() => {
-                                knex('sotinchichophep').where({ idSV: request.headers.authorization })
+                                knex('sotinchichophep')
+                                    .where({ idSV: request.headers.authorization })
                                     .update({ soTinChi: a[0].soTinChi - b[0].soTinChi })
                                     .then(() => {
                                         response.json({ success: true });
@@ -56,17 +65,25 @@ app.post('/subject/:id/register/phase1', (request, response) => {
 
 app.post('/subject/:id/register/phase2', (request, response) => {
     getLimit(request.params.id, x => {
-        knex('dangkidot2').select(knex.raw('count(*) as count')).where({ idMonHoc: request.params.id })
+        knex('dangkidot2')
+            .select(knex.raw('count(*) as count'))
+            .where({ idMonHoc: request.params.id })
             .then(r => {
                 if (x[0].soLuongSV - r[0].count > 0) {
-                    knex('sotinchichophep').select('soTinChi').where({ idSV: request.headers.authorization })
+                    knex('sotinchichophep')
+                        .select('soTinChi')
+                        .where({ idSV: request.headers.authorization })
                         .then(a => {
-                            knex('monhoc').select('soTinChi').where({ id: request.params.id })
+                            knex('monhoc')
+                                .select('soTinChi')
+                                .where({ id: request.params.id })
                                 .then(b => {
                                     if (a[0].soTinChi >= b[0].soTinChi) {
-                                        knex('dangkidot2').insert({ idMonHoc: request.params.id, idSV: request.headers.authorization })
+                                        knex('dangkidot2')
+                                            .insert({ idMonHoc: request.params.id, idSV: request.headers.authorization })
                                             .then(() => {
-                                                knex('sotinchichophep').where({ idSV: request.headers.authorization })
+                                                knex('sotinchichophep')
+                                                    .where({ idSV: request.headers.authorization })
                                                     .update({ soTinChi: a[0].soTinChi - b[0].soTinChi })
                                                     .then(() => {
                                                         response.json({ success: true });
@@ -85,7 +102,9 @@ app.post('/subject/:id/register/phase2', (request, response) => {
 
 app.get('/subjects/registered/phase1', (request, response) => {
     let user_id = request.headers.authorization;
-    knex('dangkidot1').select().where({ idSV: user_id })
+    knex('dangkidot1')
+        .select()
+        .where({ idSV: user_id })
         .then(result => {
             response.json({ success: true, data: result })
         })
@@ -93,7 +112,9 @@ app.get('/subjects/registered/phase1', (request, response) => {
 
 app.get('/subjects/registered/phase2', (request, response) => {
     let user_id = request.headers.authorization;
-    knex('dangkidot2').select().where({ idSV: user_id })
+    knex('dangkidot2')
+        .select()
+        .where({ idSV: user_id })
         .then(result => {
             response.json({ success: true, data: result })
         })
@@ -101,13 +122,20 @@ app.get('/subjects/registered/phase2', (request, response) => {
 
 
 app.post('/subject/:id/register/cancel/phase1', (request, response) => {
-    knex('sotinchichophep').select('soTinChi').where({ idSV: request.headers.authorization })
+    knex('sotinchichophep')
+        .select('soTinChi')
+        .where({ idSV: request.headers.authorization })
         .then(a => {
-            knex('monhoc').select('soTinChi').where({ id: request.params.id })
+            knex('monhoc')
+                .select('soTinChi')
+                .where({ id: request.params.id })
                 .then(b => {
-                    knex('dangkidot1').where({ idMonHoc: request.params.id, idSV: request.headers.authorization }).del()
+                    knex('dangkidot1')
+                        .where({ idMonHoc: request.params.id, idSV: request.headers.authorization })
+                        .del()
                         .then(() => {
-                            knex('sotinchichophep').where({ idSV: request.headers.authorization })
+                            knex('sotinchichophep')
+                                .where({ idSV: request.headers.authorization })
                                 .update({ soTinChi: a[0].soTinChi + b[0].soTinChi })
                                 .then(() => {
                                     response.json({ success: true });
@@ -117,13 +145,20 @@ app.post('/subject/:id/register/cancel/phase1', (request, response) => {
         })
 })
 app.post('/subject/:id/register/cancel/phase2', (request, response) => {
-    knex('sotinchichophep').select('soTinChi').where({ idSV: request.headers.authorization })
+    knex('sotinchichophep')
+        .select('soTinChi')
+        .where({ idSV: request.headers.authorization })
         .then(a => {
-            knex('monhoc').select('soTinChi').where({ id: request.params.id })
+            knex('monhoc')
+                .select('soTinChi')
+                .where({ id: request.params.id })
                 .then(b => {
-                    knex('dangkidot2').where({ idMonHoc: request.params.id, idSV: request.headers.authorization }).del()
+                    knex('dangkidot2')
+                        .where({ idMonHoc: request.params.id, idSV: request.headers.authorization })
+                        .del()
                         .then(() => {
-                            knex('sotinchichophep').where({ idSV: request.headers.authorization })
+                            knex('sotinchichophep')
+                                .where({ idSV: request.headers.authorization })
                                 .update({ soTinChi: a[0].soTinChi + b[0].soTinChi })
                                 .then(() => {
                                     response.json({ success: true });
@@ -148,12 +183,20 @@ app.get('/subjects/available/phase1', (request, response) => {
             .where({ idSV: request.headers.authorization }))
         .whereNotIn('mh.id',
             // lay hp (co hp tien quyet) ma chua hoc hoc phan tien quyet
-            knex('monhoc').select('id').innerJoin('tienquyet', 'monhoc.id', 'tienquyet.idMonSau').whereNotIn('id',
+            knex('monhoc')
+                .select('id')
+                .innerJoin('tienquyet', 'monhoc.id', 'tienquyet.idMonSau')
+                .whereNotIn('id',
 
-                // lay hp (co hp tien quyet) ma da hoc hoc phan tien quyet
-                knex('monhoc').select('id').whereIn('id',
-                    knex.select('idMonSau').from('tienquyet').innerJoin('diem', 'tienquyet.idMonTruoc', 'diem.idMonHoc')
-                        .where({ idSV: request.headers.authorization })))
+                    // lay hp (co hp tien quyet) ma da hoc hoc phan tien quyet
+                    knex('monhoc')
+                        .select('id')
+                        .whereIn('id',
+                            knex
+                                .select('idMonSau')
+                                .from('tienquyet')
+                                .innerJoin('diem', 'tienquyet.idMonTruoc', 'diem.idMonHoc')
+                                .where({ idSV: request.headers.authorization })))
 
         ).then(t => {
             response.json({ success: true, data: t })
@@ -174,12 +217,20 @@ app.get('/subjects/available/phase2', (request, response) => {
             .where({ idSV: request.headers.authorization }))
         .whereNotIn('mh.id',
             // lay hp (co hp tien quyet) ma chua hoc hoc phan tien quyet
-            knex('monhoc').select('id').innerJoin('tienquyet', 'monhoc.id', 'tienquyet.idMonSau').whereNotIn('id',
+            knex('monhoc')
+                .select('id')
+                .innerJoin('tienquyet', 'monhoc.id', 'tienquyet.idMonSau')
+                .whereNotIn('id',
 
-                // lay hp (co hp tien quyet) ma da hoc hoc phan tien quyet
-                knex('monhoc').select('id').whereIn('id',
-                    knex.select('idMonSau').from('tienquyet').innerJoin('diem', 'tienquyet.idMonTruoc', 'diem.idMonHoc')
-                        .where({ idSV: request.headers.authorization })))
+                    // lay hp (co hp tien quyet) ma da hoc hoc phan tien quyet
+                    knex('monhoc')
+                        .select('id')
+                        .whereIn('id',
+                            knex
+                                .select('idMonSau')
+                                .from('tienquyet')
+                                .innerJoin('diem', 'tienquyet.idMonTruoc', 'diem.idMonHoc')
+                                .where({ idSV: request.headers.authorization })))
 
         ).then(t => {
             // console.log(t);
@@ -209,7 +260,8 @@ app.get('/roadmap', (request, response) => {
             })
             knex('monhoc')
                 .select('id', 'ten')
-                .whereNotIn('id', knex('tienquyet').select('idMonTruoc'))
+                .whereNotIn('id', knex('tienquyet')
+                    .select('idMonTruoc'))
                 .then(arrayRoot => {
                     const arrayNested = _.map(arrayLv1, item => {
                         item.tienquyet = _.map(item.tienquyet, tienquyet => {
@@ -230,46 +282,49 @@ app.get('/roadmap', (request, response) => {
 
 app.post('/move/phase2', (request, response) => {
     // knex('dangkidot1').select('idMonHoc')
-    knex('dangkidot1').select('idMonHoc').groupBy('idMonHoc').then(ls => {
-        ls.forEach(l => {
-            getLimit(l.idMonHoc, limit_ => {
-                knex.select('monhoc.id as idmonhoc', 'monhoc.ten as tenmonhoc', 'monhoc.soTinChi as stc', 'monhoc.soLuongSV as soluongsv', 't3.idSV as idsv', 't3.dtb')
-                    .from('monhoc')
-                    .join(
+    knex('dangkidot1')
+        .select('idMonHoc')
+        .groupBy('idMonHoc')
+        .then(ls => {
+            ls.forEach(l => {
+                getLimit(l.idMonHoc, limit_ => {
+                    knex.select('monhoc.id as idmonhoc', 'monhoc.ten as tenmonhoc', 'monhoc.soTinChi as stc', 'monhoc.soLuongSV as soluongsv', 't3.idSV as idsv', 't3.dtb')
+                        .from('monhoc')
+                        .join(
 
-                        knex.select('dangkidot1.idMonHoc', 'dangkidot1.idSV as idsv', 't2.dtb')
-                            .from('dangkidot1')
-                            .join(
+                            knex.select('dangkidot1.idMonHoc', 'dangkidot1.idSV as idsv', 't2.dtb')
+                                .from('dangkidot1')
+                                .join(
 
-                                //tinh diem trung binh
-                                knex.select('t1.idSV', knex.raw('sum(diemt)/sum(soTinChi) as dtb'))
-                                    .from(function () {
-                                        this.select('diem.idSV', 'diem.diem', 'monhoc.id as idmonhoc', 'monhoc.soTinChi',
-                                            knex.raw('diem.diem * monhoc.soTinChi as diemt'))
-                                            .from('monhoc').join('diem', 'monhoc.id', 'diem.idMonHoc').as('t1')
-                                    }).groupBy('t1.idSV').as('t2'),
+                                    //tinh diem trung binh
+                                    knex.select('t1.idSV', knex.raw('sum(diemt)/sum(soTinChi) as dtb'))
+                                        .from(function () {
+                                            this.select('diem.idSV', 'diem.diem', 'monhoc.id as idmonhoc', 'monhoc.soTinChi',
+                                                knex.raw('diem.diem * monhoc.soTinChi as diemt'))
+                                                .from('monhoc').join('diem', 'monhoc.id', 'diem.idMonHoc')
+                                                .as('t1')
+                                        }).groupBy('t1.idSV').as('t2'),
 
-                                't2.idSV', 'dangkidot1.idSV')
-                            .where({ idMonHoc: l.idMonHoc })
-                            .as('t3')
-                        // end tinh dtb
+                                    't2.idSV', 'dangkidot1.idSV')
+                                .where({ idMonHoc: l.idMonHoc })
+                                .as('t3')
+                            // end tinh dtb
+                            , 'monhoc.id', 't3.idMonHoc')
+                        .limit(limit_[0].soLuongSV)
+                        .orderBy('dtb', 'desc')
 
-
-                        , 'monhoc.id', 't3.idMonHoc')
-                    .limit(limit_[0].soLuongSV)
-                    .orderBy('dtb', 'desc')
-
-                    .then(xs => {
-                        xs.forEach(x => {
-                            knex('dangkidot2').insert({ idMonHoc: x.idmonhoc, idSV: x.idsv })
-                                .then(() => {
-                                    response.json({ success: true })
-                                })
+                        .then(xs => {
+                            xs.forEach(x => {
+                                knex('dangkidot2')
+                                    .insert({ idMonHoc: x.idmonhoc, idSV: x.idsv })
+                                    .then(() => {
+                                        response.json({ success: true })
+                                    })
+                            })
                         })
-                    })
+                })
             })
         })
-    })
 
 
 })
